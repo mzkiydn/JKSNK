ff876b8638af08a5b66285236b633088b275009f
Muhammad Zakiyuddin <m.zakiyuddin@gmail.com>
2026-05-16 07:32:44 +0800

    Inital menu, page and nav

--- administrator/logs/joomla_scheduler.php
+++ administrator/logs/joomla_scheduler.php
@@ -12,3 +12,17 @@
 2026-05-12	13:46:59	INFO	Task> Could not instantiate mail function.
 2026-05-12	13:46:59	INFO	Task> UpdateNotification end
 2026-05-12	13:46:59	INFO	Successfully finished task#03 in 5.90 (net 5.90) seconds.
+2026-05-14	02:54:09	INFO	Running task#02 'Session GC'.
+2026-05-14	02:54:09	INFO	Task> SessionGC end
+2026-05-14	02:54:10	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2026-05-14	02:54:18	INFO	Running task#03 'Update Notification'.
+2026-05-14	02:54:30	INFO	Task> Could not instantiate mail function.
+2026-05-14	02:54:30	INFO	Task> UpdateNotification end
+2026-05-14	02:54:30	INFO	Successfully finished task#03 in 11.25 (net 11.26) seconds.
+2026-05-15	07:37:30	INFO	Running task#02 'Session GC'.
+2026-05-15	07:37:30	INFO	Task> SessionGC end
+2026-05-15	07:37:31	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2026-05-15	07:37:49	INFO	Running task#03 'Update Notification'.
+2026-05-15	07:38:00	INFO	Task> Could not instantiate mail function.
+2026-05-15	07:38:00	INFO	Task> UpdateNotification end
+2026-05-15	07:38:00	INFO	Successfully finished task#03 in 11.39 (net 11.39) seconds.
--- templates/yootheme/cache/theme-d358d9ad.php
+++
@@ -1,9 +0,0 @@
-<?php // $file = C:\xampp\htdocs\web\templates\yootheme\packages\styler/config/theme.json
-
-return [
-  'defaults' => [
-    'less' => [], 
-    'custom_less' => '', 
-    'style' => 'fuse'
-  ]
-];
